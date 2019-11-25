92440dd8d0f54731f2cbbf4d665442c180e02c78
Sabolc Keringer <keringersz@gmail.com>
2019-11-25 20:29:37 +0100

    Zadatak 1: Layout created

---
+++ src/components/AppPosts.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Heyy
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from "vue-router";
 import App from './App.vue'
+import AppPosts from './components/AppPosts.vue'
+
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const routes = [
+  { name: "index", path: "/", redirect: "/posts" },
+  { name: "posts", path: "/posts", component: AppPosts },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
+
